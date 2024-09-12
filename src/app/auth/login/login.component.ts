62098dcd24a81038936aa506e0dd03cc80bc901c
S. M. Rafi <smrafialam@gmail.com>
2024-09-13 02:00:49 +0600

    created login & register component for user

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink,FormsModule,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
+
+}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterLink,FormsModule,ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit{
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  onsubmit(){
+    console.log(this.registerForm.value);
+  }
+
+}
